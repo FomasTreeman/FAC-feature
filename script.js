2bdf828628ecda930d26aec6ef9aa87e89fe5d61
FomasTreeman <tom@team-freeman.com>
2022-10-26 21:14:59 -1000

    fixed new tortoises not having animation

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var racers = [1, 2, 3];
 let winner = 0;
 
 function addTortoise() {
+    const racerNum = racers.length + 1;
     var img = document.createElement("img");
     img.src = "/tortoise.png";
+    img.setAttribute("id", "racer" + racerNum);
     var para = document.createElement("p");
-    para.innerText = racers.push(racers.length + 1);
+    para.innerText = racers.push(racerNum);
     document.querySelector(".racerContainer").appendChild(para);
     document.querySelector(".racerContainer").appendChild(img);
     document.querySelector(".racerContainer").appendChild(document.createElement("hr"));
@@ -14,7 +16,6 @@ function addTortoise() {
 
 function start() {
     console.log("started");
-    console.log("racer" + 1 )
     let speeds = racers.map((racer) => setSpeed(racer))
     
     console.log(speeds);
@@ -26,6 +27,10 @@ function start() {
 
 function setSpeed(num) {
     let speed = ((Math.random() * 4) + 3); 
-    document.getElementById("racer" + num).style.animation="racing " + speed +"s linear";
+    let currentRacer =  document.getElementById("racer" + num);
+    currentRacer.style.animation = "";
+    setTimeout( function(){
+        currentRacer.style.animation="racing " + speed +"s linear";
+    }, 10);
     return speed;
 }
